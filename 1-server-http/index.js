3f5a0d12816e890f80bb3a39334c405a9727efe1
Vinicius <viniciusmaeda@gmail.com>
2025-04-01 08:00:49 -0400

    exemplo de servidor http

--- 1-server-http/index.js
+++ 1-server-http/index.js
@@ -1,1 +1,24 @@
-console.log('Este é o meu servidor Node.js');
+// Projeto Node.js com o módulo HTTP
+
+// importar o pacote http
+const http = require('http');
+
+// definir as configurações do servidor
+hostname = '127.0.0.1'; // endereço IP local
+port = 3000; // porta do servidor
+
+// criar o servidor
+const server = http.createServer((requisicao, resposta) => {
+  // definir o cabeçalho da resposta
+  resposta.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // enviar a resposta
+  resposta.end('Hello World');
+});
+
+// iniciar o servidor
+server.listen(port, hostname, () => {
+  console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
+
+
