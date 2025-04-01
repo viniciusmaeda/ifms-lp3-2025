e50471c1576cc43e11a1d6e7854a227881402094
Vinicius <viniciusmaeda@gmail.com>
2025-04-01 08:16:14 -0400

    alterações do servidor http

--- 1-server-http/index.js
+++ 1-server-http/index.js
@@ -10,15 +10,19 @@ port = 3000; // porta do servidor
 // criar o servidor
 const server = http.createServer((requisicao, resposta) => {
   // definir o cabeçalho da resposta
-  resposta.writeHead(200, { 'Content-Type': 'text/plain' });
+  resposta.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+
+  resposta.write('Olá este é o meu primeiro servidor Node.js!\n');
+  resposta.write('Ele está rodando na porta 3000.\n');
+  resposta.write('Está utilizando o módulo http.\n');
+  resposta.write('Toda alteração deve ser reiniciado o servidor.');
 
   // enviar a resposta
-  resposta.end('Hello World');
+  resposta.end();
 });
 
 // iniciar o servidor
 server.listen(port, hostname, () => {
   console.log(`Servidor rodando em http://${hostname}:${port}/`);
 });
-
-
+// Fazer uma requisição HTTP
